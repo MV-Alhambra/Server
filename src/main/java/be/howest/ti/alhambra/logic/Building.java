08f8fba7a0faea2e7161a5d11a946520018bb862
maxim <maxim.decherf@student.howest.be>
2020-04-28 18:35:39 +0200

    removed all the code for making random building bcs you there is a list of useable buildings ... :/

--- src/main/java/be/howest/ti/alhambra/logic/Building.java
+++ src/main/java/be/howest/ti/alhambra/logic/Building.java
@@ -8,45 +8,11 @@ public class Building {
     private final Map<WindDirection, Boolean> walls;
 
 
-    Random random = new Random(); //so you can reuse it and need to make a new object each time
-    List<BuildingType> allTypes = Arrays.asList(BuildingType.values()); //so you can reuse it and need to make a new object each time
 
     public Building(){
-        this.type = randomType();
-        this.cost = randomCost();
-        this.walls = generateWalls();
-    }
-
-    private BuildingType randomType() {
-        int randomBuildingIndex = random.nextInt(allTypes.size());
-        return allTypes.get(randomBuildingIndex);
-    }
-
-    private int randomCost(){
-        int baseNumber = allTypes.indexOf(type);
-        int min = baseNumber + 2;
-        int max = min + 6;
-
-        return  random.nextInt(max) + min; // nextInt is from 0 to bound, the +min makes it goes from min incl to max excl
-    }
-
-    private Map<WindDirection, Boolean> generateWalls(){
-
-        Map<WindDirection, Boolean> wallsGenerate = new HashMap<>();
-        int currentWalls = 0;
-
-        for (WindDirection direction  : WindDirection.values()) {  //this makes it so there are no more then 3 walls
-            boolean wall = random.nextBoolean();
-            if (wall && (currentWalls < 3)){
-                currentWalls ++;
-                wallsGenerate.put(direction, true);
-            }
-            else {
-                wallsGenerate.put(direction, false);
-            }
-
-        }
-        return wallsGenerate;
+        this.type = null;
+        this.cost = 0;
+        this.walls = null;
     }
 
     public BuildingType getType() {
