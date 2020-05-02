8877b5c4f324741337ce12f235827fe01f3ce7c7
Simon <simon.plyson@student.howest.be>
2020-05-02 15:03:38 +0200

    Finished market coding

--- src/main/java/be/howest/ti/alhambra/logic/Market.java
+++ src/main/java/be/howest/ti/alhambra/logic/Market.java
@@ -1,4 +1,24 @@
 package be.howest.ti.alhambra.logic;
 
-public class Market {
+import java.util.Map;
+
+public class Market
+{
+    //Replace String with Building
+    private Map<Currency,String> market;
+
+    public void addBuilding(Currency currency, String building)
+    {
+        market.put(currency, building);
+    }
+
+    public String getBuilding (Currency currency)
+    {
+        return market.get(currency);
+    }
+
+    public void removeBuilding(Currency currency)
+    {
+        market.remove(currency);
+    }
 }
