9d84c87f5ab998863a95b0dde729c21f5be3cc41
mitch <mitch.rollez@student.howest.be>
2020-05-02 13:26:56 +0200

    Add constructor

--- src/main/java/be/howest/ti/alhambra/logic/Reserve.java
+++ src/main/java/be/howest/ti/alhambra/logic/Reserve.java
@@ -1,12 +1,16 @@
 package be.howest.ti.alhambra.logic;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class Reserve {
-    public static void main(String[] args) {
-        new Reserve().run();
-    }
+    private final List<String> reserve;
 
-    void run() {
-
+    @JsonCreator
+    public Reserve(@JsonProperty("reserve") List<String> reserve){
+        this.reserve = reserve;
     }
 
 }
