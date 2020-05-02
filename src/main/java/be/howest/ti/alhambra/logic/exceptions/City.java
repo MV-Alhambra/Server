803c9631d76cbbf7d67b6828dc329da42e3e47f1
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-05-02 20:20:07 +0200

    Add JSON to city

--- src/main/java/be/howest/ti/alhambra/logic/exceptions/City.java
+++ src/main/java/be/howest/ti/alhambra/logic/exceptions/City.java
@@ -1,16 +1,24 @@
 package be.howest.ti.alhambra.logic.exceptions;
 
 import be.howest.ti.alhambra.logic.Building;
+import com.fasterxml.jackson.annotation.*;
 
 public class City {
 
-    private static final Building[][] defaultBuildings = { {null,null,null},{null,new Building(null,0),null},{null,null,null}};
+    private static final Building[][] defaultCity = {{null, null, null}, {null, new Building(null, 0), null}, {null, null, null}};
     private Building[][] buildings;
 
-    public City(Building[][] buildings) {
+    @JsonCreator
+    public City(@JsonProperty("city") Building[][] buildings) {
         this.buildings = buildings;
     }
-    public City(){
-        this(City.defaultBuildings);
+
+    public City() {
+        this(City.defaultCity);
+    }
+
+    @JsonGetter("city")
+    public Building[][] getBuildings() {
+        return buildings;
     }
 }
