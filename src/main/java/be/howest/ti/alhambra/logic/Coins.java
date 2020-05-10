13bc347f367d2e41b7d18bef1cd8268fae3a382e
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-05-10 19:52:50 +0200

    Add containsCoins method to Coins

--- src/main/java/be/howest/ti/alhambra/logic/Coins.java
+++ src/main/java/be/howest/ti/alhambra/logic/Coins.java
@@ -1,19 +1,24 @@
 package be.howest.ti.alhambra.logic;
 
-import com.fasterxml.jackson.annotation.*;
-import java.util.*;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Coins {
 
     private final List<Coin> coinsBag;
 
+    public Coins() {
+        this(new ArrayList<>());
+    }
+
     @JsonCreator
     public Coins(@JsonProperty("coins") List<Coin> coins) {
         this.coinsBag = coins;
-    }
-
-    public Coins() {
-        this(new ArrayList<>());
     }
 
     public void addCoins(Coin[] coins) {
@@ -36,6 +41,14 @@ public class Coins {
         if (!coinsBag.remove(coin)) throw new IllegalArgumentException("Couldn't find the coin in the coins bag, coin: " + coin.toString());
     }
 
+    public boolean containsCoins(Coin[] coins) {
+        List<Coin> copyCoinsBag = new ArrayList<>(coinsBag);
+        for (Coin coin : coins) {
+            if (!copyCoinsBag.remove(coin)) return false;
+        }
+        return true;
+    }
+
     public boolean containsCoin(Coin coin) {
         return coinsBag.contains(coin);
     }
@@ -46,15 +59,15 @@ public class Coins {
     }
 
     @Override
+    public int hashCode() {
+        return Objects.hash(coinsBag);
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Coins coins1 = (Coins) o;
         return Objects.equals(coinsBag, coins1.coinsBag);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(coinsBag);
     }
 }
