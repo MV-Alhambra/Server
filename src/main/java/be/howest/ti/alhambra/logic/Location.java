dfb827965b89fec27ec8bfd24c22a82f5f650578
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-05-11 13:43:30 +0200

    Switch row and col in Location

--- src/main/java/be/howest/ti/alhambra/logic/Location.java
+++ src/main/java/be/howest/ti/alhambra/logic/Location.java
@@ -5,23 +5,24 @@ import com.fasterxml.jackson.annotation.*;
 import java.util.Objects;
 
 public class Location {
-    private final int col;
     private final int row;
+    private final int col;
+
 
     @JsonCreator
-    public Location(@JsonProperty("col") int col, @JsonProperty("row") int row) {
-        this.col = col;
+    public Location (@JsonProperty("row") int row,@JsonProperty("col") int col) {
         this.row = row;
+        this.col = col;
     }
 
     public static Location convertLocationToStaticLocation(Location location, int mapSize) { //turns the dynamic location/location based around fountain into location based on top left
         int mapRadius = (mapSize - 1) / 2;
-        return new Location(location.getCol() + mapRadius, location.getRow() + mapRadius);
+        return new Location(location.getRow() + mapRadius,location.getCol() + mapRadius);
     }
 
     public static Location convertStaticLocationToLocation(Location location, int mapSize) { //turns the static location on based on top left into dynamic location based on fountain position
         int mapRadius = (mapSize - 1) / 2;
-        return new Location(location.getCol() - mapRadius, location.getRow() - mapRadius);
+        return new Location(location.getRow() - mapRadius,location.getCol() - mapRadius);
     }
 
     public int getCol() {
@@ -47,6 +48,6 @@ public class Location {
 
     @Override
     public String toString() {
-        return String.format("(%d;%d)",col,row);
+        return String.format("(%d;%d)",row, col);
     }
 }
