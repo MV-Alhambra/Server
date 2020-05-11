5701e1cf18bfb78ee0ca9c5f9738fe6f97859e32
Simon <simon.plyson@student.howest.be>
2020-05-11 16:31:19 +0200

    add equals for customLobbyName and maxNumberOfPlayers

--- src/main/java/be/howest/ti/alhambra/logic/Lobby.java
+++ src/main/java/be/howest/ti/alhambra/logic/Lobby.java
@@ -149,15 +149,20 @@ public class Lobby {
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(id, playersReady);
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Lobby lobby = (Lobby) o;
-        return Objects.equals(id, lobby.id);
+        return maxNumberOfPlayers == lobby.maxNumberOfPlayers &&
+                playerCount == lobby.playerCount &&
+                readyCount == lobby.readyCount &&
+                Objects.equals(id, lobby.id) &&
+                Objects.equals(customNameLobby, lobby.customNameLobby) &&
+                Objects.equals(playersReady, lobby.playersReady);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(maxNumberOfPlayers, id, customNameLobby, playersReady, playerCount, readyCount);
     }
 }
