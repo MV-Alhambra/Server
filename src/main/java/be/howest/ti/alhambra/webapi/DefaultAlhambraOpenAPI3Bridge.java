e922be6117e82ad802d43a07c0506c0da4bee0c3
Simon <simon.plyson@student.howest.be>
2020-05-11 15:58:25 +0200

    add maxNumberOfPlayers to create game

--- src/main/java/be/howest/ti/alhambra/webapi/DefaultAlhambraOpenAPI3Bridge.java
+++ src/main/java/be/howest/ti/alhambra/webapi/DefaultAlhambraOpenAPI3Bridge.java
@@ -57,7 +57,7 @@ public class DefaultAlhambraOpenAPI3Bridge implements AlhambraOpenAPI3Bridge {
 
     public Object createGame(RoutingContext ctx) {
         LOGGER.info("createGame");
-        return controller.addLobby(ctx.getBodyAsJson().getValue("customGameName").toString());
+        return controller.addLobby(ctx.getBodyAsJson().getValue("customGameName").toString(),ctx.getBodyAsJson().getInteger("maxNumberOfPlayers"));
     }
 
     public Object clearGames(RoutingContext ctx) {
