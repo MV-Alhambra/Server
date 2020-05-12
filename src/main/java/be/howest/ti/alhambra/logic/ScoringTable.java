dfa2678bf93f9e00bf27ef0247144666c8af0f2f
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-05-13 00:07:38 +0200

    Add getRoundTable Method

--- src/main/java/be/howest/ti/alhambra/logic/ScoringTable.java
+++ src/main/java/be/howest/ti/alhambra/logic/ScoringTable.java
@@ -1,7 +1,12 @@
 package be.howest.ti.alhambra.logic;
 
+import be.howest.ti.alhambra.logic.exceptions.AlhambraIllegalArgumentException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 //Utility class to calc the score, everything the do with score should be put here
 public class ScoringTable {
@@ -9,19 +14,25 @@ public class ScoringTable {
     private ScoringTable() {
     } //bc you aren't allowed to make instances of utility classes
 
-    public static Map<BuildingType, List<Integer>> getRoundTable(int round) {
-        if (1 > round || round > 3) throw new IllegalArgumentException("only rounds between 1 and 3 allowed"); //defense programming
-
-        //Dynamically calculate the round table, it's round + increasing by 1 for each BuildingType
-        for (BuildingType type: BuildingType.values()){
-            System.out.println(type);
-        }
-
-        return null;
+    public static void main(String[] args) {
+        System.out.println(ScoringTable.getRoundTable(2));
     }
 
-    public static void main(String[] args) {
-        ScoringTable.getRoundTable(1);
+    public static Map<BuildingType, List<Integer>> getRoundTable(int round) {
+        if (1 > round || round > 3) throw new AlhambraIllegalArgumentException("only rounds between 1 and 3 allowed"); //defense programming
+
+        Map<BuildingType, List<Integer>> roundTable = new HashMap<>();
+        //Dynamically calculate the round table, it's round + increasing by 1 for each BuildingType
+        for (BuildingType type : BuildingType.values()) { // set the initial values
+            roundTable.put(type, new ArrayList<>());
+        }
+        AtomicInteger index = new AtomicInteger();
+        for (int i = 1; i <= round; i++) { //fill them
+            int finalI = i;
+            if (i == 3) index.getAndIncrement();
+            roundTable.forEach((key, value) -> value.add(0, index.getAndIncrement() + finalI));
+        }
+        return roundTable;
     }
 
 }
