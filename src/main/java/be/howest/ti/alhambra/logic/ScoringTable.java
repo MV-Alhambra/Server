1f7a52b253d59a918fb757a19eafdd4be8510cc1
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-05-13 12:39:31 +0200

    Refactor some code in ScoringTable

--- src/main/java/be/howest/ti/alhambra/logic/ScoringTable.java
+++ src/main/java/be/howest/ti/alhambra/logic/ScoringTable.java
@@ -13,26 +13,22 @@ public class ScoringTable {
 
     public static Map<Player, Integer> calcScoreBuildings(List<Player> players, int round) {
 
-        Map<BuildingType, Map<Player, Integer>> totalTypeEachPlayer = new LinkedHashMap<>();    //map with for each type another map with how key player and then how many they have  of that type, linked bc order matters
+        Map<BuildingType, Map<Player, Integer>> totalTypeEachPlayer = new LinkedHashMap<>();// map with for each type another map with key player and then how many they have of that type, linked bc order matters
 
         for (BuildingType type : BuildingType.values()) {
             totalTypeEachPlayer.put(type, new LinkedHashMap<>()); //order matters
-            for (Player player : players) { //fill the map with players and count for each type
-                totalTypeEachPlayer.get(type).put(player, player.getCity().countType(type));
-            }
+
+            players.forEach(player -> totalTypeEachPlayer.get(type).put(player, player.getCity().countType(type)));//fill the map with players and count for each type
+
             totalTypeEachPlayer.put(type, totalTypeEachPlayer.get(type).entrySet().stream() //sort players by count of each type
-                    .filter(k -> k.getValue() > 0)
+                    .filter(k -> k.getValue() > 0) //ppl with no buildings for that type dont get points
                     .sorted(Map.Entry.comparingByValue((i1, i2) -> i2 - i1)) //sort them by custom comparator cuz i wanted it reversed, that code in the parameter is a comparator
                     .limit(round) //only hold amount of players equal to scores available
                     .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new)) //back to map
             );
         }
-
         Map<Player, Integer> scores = new HashMap<>();
-
-        for (Player player : players) {
-            scores.put(player, 0);
-        }
+        players.forEach(player -> scores.put(player, 0)); //init values
         Map<BuildingType, List<Integer>> roundTable = getRoundTable(round);
 
         for (Map.Entry<BuildingType, Map<Player, Integer>> type : totalTypeEachPlayer.entrySet()) {
