0105080f971158458da108aff7c4a9339f7fdbfb
mitch <mitch.rollez@student.howest.be>
2020-05-20 10:42:01 +0200

    Add coin test class

---
+++ src/test/java/be/howest/ti/alhambra/logic/CoinTest.java
@@ -0,0 +1,15 @@
+package be.howest.ti.alhambra.logic;
+
+import org.junit.jupiter.api.*;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CoinTest {
+
+    @Test
+    void coin() {
+        
+    }
+}
