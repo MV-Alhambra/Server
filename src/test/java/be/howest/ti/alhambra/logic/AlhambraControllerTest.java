61881711fc1dbdab42abe818a784213c3ee093ef
mitch <mitch.rollez@student.howest.be>
2020-05-20 11:13:02 +0200

    Add AlhambraControllerTest file

---
+++ src/test/java/be/howest/ti/alhambra/logic/AlhambraControllerTest.java
@@ -0,0 +1,14 @@
+package be.howest.ti.alhambra.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AlhambraControllerTest {
+
+    @Test
+    void AlhambraCont() {
+
+
+    }
+}
