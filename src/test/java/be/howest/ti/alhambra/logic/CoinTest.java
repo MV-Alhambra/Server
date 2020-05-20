50638e38e0f52960913b42a707e5510904d29599
mitch <mitch.rollez@student.howest.be>
2020-05-20 10:56:17 +0200

    Add test for coinsSameCurrency function

--- src/test/java/be/howest/ti/alhambra/logic/CoinTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/CoinTest.java
@@ -13,9 +13,20 @@ public class CoinTest {
         Coin coin1 = new Coin(Currency.GREEN, 3);
         Coin coin2 = new Coin(Currency.BLUE, 9);
         Coin coin3 = new Coin(Currency.ORANGE, 11);
+        Coin coin4 = new Coin(Currency.BLUE, 6);
 
         Coin[] coins = new Coin[]{coin1, coin2, coin3};
 
         assertEquals(23, Coin.getSumCoins(coins) );
+
+
+        coins = new Coin[]{coin1};
+        assertTrue(Coin.coinsSameCurrency(coins));
+
+        coins = new Coin[]{coin1, coin2};
+        assertFalse(Coin.coinsSameCurrency(coins));
+
+        coins = new Coin[]{coin2, coin4};
+        assertTrue(Coin.coinsSameCurrency(coins));
     }
 }
