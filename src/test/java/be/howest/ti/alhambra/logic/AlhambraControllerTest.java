b3a58faba5edede1b21c7849834b266ad438779e
mitch <mitch.rollez@student.howest.be>
2020-05-23 14:47:55 +0200

    Add test for getLobby

--- src/test/java/be/howest/ti/alhambra/logic/AlhambraControllerTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/AlhambraControllerTest.java
@@ -2,6 +2,9 @@ package be.howest.ti.alhambra.logic;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AlhambraControllerTest {
@@ -10,7 +13,18 @@ public class AlhambraControllerTest {
     void AlhambraCont() {
         AlhambraController c = new AlhambraController();
 
+        //Test addLobby function
         assertEquals("001", c.addLobby("Test", 6, false));
+
+        assertEquals(new Building(null, -1, null), c.getBuildings());
+
+        List<Lobby> lobbylist = new ArrayList<>();
+        Lobby l = new Lobby("001" , "Test", 6, false);
+        lobbylist.add(l);
+
+        assertEquals(lobbylist, c.getLobbies());
+
+
 
     }
 }
